d0b333829dea00eca18f103d52cef114da0fc859
Deiner <deiner.ra10@gmail.com>
2020-09-21 20:34:57 -0800

    se agrego una funcion save que sirve para guardar Pesona en un archivo binario

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,28 @@ struct Persona {
     }
 };
 
+void save(Persona persona){
+    ofstream archivo;
+    //se crea una variable ofstream para poder usar los metodos necesarios para escribir en un archivo como .open, .write , .close ,...
+
+
+    archivo.open("ArchivoBinario.dat",ios::app | ios::binary);
+    //se abre o se crea el archivo "ArchivoBinario.dat" y se decide en que modo se abrira ese archivo y por ultimo se le recuerda a C++ que es un archivo binario
+
+    if(archivo.fail()){
+        cout<<"no se pudo abrir el archivo";
+        exit(1);}
+    //si el archivo no se pudo abrir o algo mas paso el metodo fail() retorna un 1 si algo malo paso y con el exit(1) nos salimos del metodo
+
+    archivo.write((char *)&persona,sizeof(Persona));
+    //esta el metodo que se usa para escribir en el archivo binario
+    //se le pasan  dos paramentros, primero la direccion del objeto,struct,variable que se va "almacenar" y segundo el tamanno de este objeto,struct o variable
+
+
+    archivo.close();
+    //por ultimo se cierra el archivo
+}
+
 
 
 int maim{
