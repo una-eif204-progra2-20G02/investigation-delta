6441ec6fd24f73d2197668e2d4285acde0b2146f
Deiner <deiner.ra10@gmail.com>
2020-09-21 20:34:05 -0800

    se agrego una struct Persona, con su constructor sin parametros y con parametros

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 //
 // Created by deine on 9/21/2020.
 //
+#include <iostream>
+#include <fstream> //usar las funciones de escritura y lectura
+#include <strings.h> //pasar de string cadena de char
+#include <vector>
+using namespace std;
+
+
+struct Persona {
+    char nombre[20];
+    int edad;
+    int id;
+    Persona(){}
+    Persona(string _name,int _edad, int _id){
+        strcpy(nombre,_name.c_str());
+        edad=_edad;
+        id=_id;
+    }
+};
+
 
 
 int maim{
