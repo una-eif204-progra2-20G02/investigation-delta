cf5f15a944a4aecd6075038f2e94ea8d96a95996
luisolcmd <68918455+luisolcmd@users.noreply.github.com>
2020-09-21 21:36:27 -0600

    Creación del método guardar.

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,37 @@
 #include <fstream> //usar las funciones de escritura y lectura
 #include <strings.h> //pasar de string cadena de char
 #include <vector>
+
 using namespace std;
 
 struct Persona {
     char nombre[20];
     int edad;
     int id;
-    Persona(){}
-    Persona(string _name,int _edad, int _id){
-        strcpy(nombre,_name.c_str());
-        edad=_edad;
-        id=_id;
+
+    Persona() {}
+
+    Persona(string _name, int _edad, int _id) {
+        strcpy(nombre, _name.c_str());
+        edad = _edad;
+        id = _id;
     }
 };
 
-void save(Persona persona){
+void save(Persona persona) {
     ofstream archivo;
     //se crea una variable ofstream para poder usar los metodos necesarios para escribir en un archivo como .open, .write , .close ,...
 
-    archivo.open("ArchivoBinario.dat",ios::app | ios::binary);
+    archivo.open("ArchivoBinario.dat", ios::app | ios::binary);
     //se abre o se crea el archivo "ArchivoBinario.dat" y se decide en que modo se abrira ese archivo y por ultimo se le recuerda a C++ que es un archivo binario
 
-    if(archivo.fail()){
-        cout<<"no se pudo abrir el archivo";
-        exit(1);}
+    if (archivo.fail()) {
+        cout << "no se pudo abrir el archivo";
+        exit(1);
+    }
     //si el archivo no se pudo abrir o algo mas paso el metodo fail() retorna un 1 si algo malo paso y con el exit(1) nos salimos del metodo
 
-    archivo.write((char *)&persona,sizeof(Persona));
+    archivo.write((char *) &persona, sizeof(Persona));
     //esta el metodo que se usa para escribir en el archivo binario
     //se le pasan  dos paramentros, primero la direccion del objeto,struct,variable que se va "almacenar" y segundo el tamanno de este objeto,struct o variable
 
@@ -39,70 +43,87 @@ void save(Persona persona){
     //por ultimo se cierra el archivo
 }
 
-void load(Persona& persona){
+void load(Persona &persona) {
     ifstream archivo;
     //se crea una variable ifstream para poder usar los metodos necesarios para leer el archivo como .open, .read , .close ,...
 
-    archivo.open("ArchivoBinario.dat",ios::in | ios::binary);
+    archivo.open("ArchivoBinario.dat", ios::in | ios::binary);
     //carga el archivo, se decide en que modo se va a abrir ese archivo( en este caso en modo lectura) y se le recuerda a C++ que es un archivo binario
 
-    if(archivo.fail()){
-        cout<<"no se pudo abrir el archivo";
+    if (archivo.fail()) {
+        cout << "no se pudo abrir el archivo";
         exit(1);
     }
     //si el archivo no se pudo abrir o algo mas paso el metodo fail() retorna un 1 si algo malo paso y con el exit(1) nos salimos del metodo
 
-    archivo.read((char *)&persona, sizeof(Persona));
+    archivo.read((char *) &persona, sizeof(Persona));
     //lo que esta en el archivo se va a cargar en la persona
 
     archivo.close();
     //se cerro el archivo
 }
 
-void ejemploSeekgYTellg(Persona& persona){
+void ejemploSeekgYTellg(Persona &persona) {
     ifstream archivo;
 
-    archivo.open("ArchivoBinario.dat",ios::in | ios::binary);
+    archivo.open("ArchivoBinario.dat", ios::in | ios::binary);
     //carga el archivo, se decide en que modo se va a abrir ese archivo( en este caso en modo lectura) y se le recuerda a C++ que es un archivo binario
 
-    if(archivo.fail()){
-        cout<<"no se pudo abrir el archivo";
+    if (archivo.fail()) {
+        cout << "no se pudo abrir el archivo";
         exit(1);
     }
     //si el archivo no se pudo abrir o algo mas paso el metodo fail() retorna un 1 si algo malo paso y con el exit(1) nos salimos del metodo
 
     //por ejemplo queremos mostrar la tercera persona que fuen ingresado en el archivo
-    archivo.seekg(2*sizeof(Persona));
+    archivo.seekg(2 * sizeof(Persona));
     //ya que C++ utiliza lo que son direccion fisicas lo cual quiere decir que se recorre el archivo byte a byte
     //por ese motivo hay multiplicarle el sizeof(Persona) para ubicarnos en la tercera persona
     //lo multiplicamos por 2 ya que la primera persona es el registro 0, la segunda el 1, la tercera el 2,...
 
-    archivo.read((char *)&persona,sizeof(Persona));
-    cout<<"La lectura del archivo quedo en el byte: "<<archivo.tellg();
+    archivo.read((char *) &persona, sizeof(Persona));
+    cout << "La lectura del archivo quedo en el byte: " << archivo.tellg();
 
 
     archivo.close();
     //se cerro el archivo
 }
 
-int main(){
-    Persona p1("Luis",23,1);
-    Persona p2("Christofer",19,2);
-    Persona p3("Deyner",43,3);
-    Persona p4("Maikol",45,4);
+void guardar(Persona persona) {
+    ofstream archivo;
+    archivo.open("ArchivoTexto.txt", ios::app);
+    if (archivo.fail()) {
+        cout << "Error al crear archivo";
+    }
+    archivo << persona.nombre << ", " << persona.edad << ", " << persona.id << endl;
+    archivo.close();
+}
 
+int main() {
+    Persona p1("Luis", 23, 1);
+    Persona p2("Christofer", 19, 2);
+    Persona p3("Deiner", 43, 3);
+    Persona p4("Maikol", 45, 4);
+
+    Persona aux;
+    load(aux);
+    cout << aux.id << endl;
+    cout << endl;
+    ejemploSeekgYTellg(aux);
+    cout << endl;
+    cout << aux.nombre << " " << aux.edad << endl;
+
+    //Archivo binario
     save(p1);
     save(p2);
     save(p3);
     save(p4);
 
-    Persona aux;
-    load(aux);
-    cout<<aux.id<<endl;
-    cout<<endl;
-    ejemploSeekgYTellg(aux);
-    cout<<endl;
-    cout<<aux.nombre<<" "<<aux.edad<<endl;
+    //Archivo de texto
+    guardar(p1);
+    guardar(p2);
+    guardar(p3);
+    guardar(p4);
 
     return 0;
 };
